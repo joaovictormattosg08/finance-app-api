1aca555413c2945bbe853529ef8070c1501fe816
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-06-07 21:46:32 -0300

    feat: add init migrations to create initial tables

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import 'dotenv/config.js'
 import express from 'express'
 
-import { PostgresHelper } from './src/db/postgres/Helper.js'
+import { PostgresHelper } from './src/db/postgres/helper.js'
 const app = express()
-
-
 
 app.get('/', async (req, res) => {
     const results = await PostgresHelper.query('SELECT * FROM users;')
