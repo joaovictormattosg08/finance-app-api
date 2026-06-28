9dea668f15c9ef468b73814895183e8e39441db6
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-06-27 23:15:45 -0300

    fix: fixed the sql query

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -2,9 +2,10 @@ import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionID) {
-        const deletedTransaction = await PostgresHelper.query(
-            'DELETE FROM transactions WHERE id = $1',
+        const transaction = await PostgresHelper.query(
+            'DELETE FROM transactions WHERE id = $1 RETURNING *',
             [transactionID],
         )
+        return transaction[0]
     }
 }
