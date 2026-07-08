b5349fe9488578a460133e4a7a4845cc844ff024
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-08 11:23:31 -0300

    feat: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -27,4 +27,23 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).not.toBeUndefined()
     })
+
+    it('Shoul reaturn 400 if first_name is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Victor',
+                email: 'joao@gmail.com',
+                password: '12345678',
+            },
+        }
+        //Act
+        const result = await createUserController.execute(httpRequest)
+
+        //Assert
+        expect(result.statusCode).toBe(400)
+    })
 })
