7edccfbd8908b5be3d01910ed739150b7d3cb2b9
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-08 12:14:57 -0300

    test: ensure 400 is return if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -103,4 +103,22 @@ describe('Create User Controller', () => {
         //Assert
         expect(result.statusCode).toBe(400)
     })
+    it('should return 400 if password is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'João',
+                last_name: 'Victor',
+                email: 'joao@gmail.com',
+            },
+        }
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
