3a2f4b44f2242541d875192aa436c83522e95b14
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-08 12:18:44 -0300

    test: ensure 400 is returned if password is less than 6 characters

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -121,4 +121,24 @@ describe('Create User Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is less than 6 characters', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'João',
+                last_name: 'Victor',
+                email: 'joao@gmail.com',
+                password: '12345',
+            },
+        }
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
