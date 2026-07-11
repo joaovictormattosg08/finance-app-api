4efe1b5e1abb1be211e0b5a24706784caf34f2a5
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-10 22:59:36 -0300

    test: ensure GetUserByIdController return 200 when finding a user

---
+++ src/controllers/user/get-user-by-id.test.js
@@ -0,0 +1,35 @@
+import { faker } from '@faker-js/faker'
+import { GetUserByIdController } from './get-user-by-id'
+
+describe('GetUserByIdController', () => {
+    class getUserByIdUseCaseStub {
+        async execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdUseCase = new getUserByIdUseCaseStub()
+        const sut = new GetUserByIdController(getUserByIdUseCase)
+
+        return { sut, getUserByIdUseCase }
+    }
+
+    it('should return 200 if a user is found', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            params: {
+                userId: faker.string.uuid(),
+            },
+        })
+
+        expect(result.statusCode).toBe(200)
+    })
+})
