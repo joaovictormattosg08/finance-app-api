ae0c4733934ef866d17373b0f4824e92d8b511c9
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-11 20:14:10 -0300

    test: ensure CreateTransactionController returns 400 if name is not provided

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -46,4 +46,17 @@ describe('CreateTransactionController', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if name is not provided', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            body: {
+                ...httpRequest,
+                name: undefined,
+            },
+        })
+
+        expect(result.statusCode).toBe(400)
+    })
 })
