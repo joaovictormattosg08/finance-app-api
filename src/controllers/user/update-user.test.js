316eac8ba38a07978c6050676fd69a3a84c40eb6
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-10 23:23:37 -0300

    test: ensure UpdateUserController returns 200 when updating a user

---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,36 @@
+import { UpdateUserController } from './update-user.js'
+import { faker } from '@faker-js/faker'
+describe('UpdateUserController', () => {
+    class UpdateUserUseCaseStub {
+        async execute(user) {
+            return user
+        }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        },
+    }
+
+    const makeSut = () => {
+        const updateUserUseCase = new UpdateUserUseCaseStub()
+        const sut = new UpdateUserController(updateUserUseCase)
+
+        return { sut, updateUserUseCase }
+    }
+
+    it('should return 200 when updating an user successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(200)
+    })
+})
