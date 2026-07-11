85ecdb85fff7833ffb333b655d6d498f04337c63
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-11 20:15:37 -0300

    test: ensure CreateTransactionController returns 400 if type is not provided

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -72,6 +72,7 @@ describe('CreateTransactionController', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
     it('should return 400 if amount is not provided', async () => {
         const { sut } = makeSut()
 
@@ -79,6 +80,19 @@ describe('CreateTransactionController', () => {
             body: {
                 ...httpRequest,
                 amount: undefined,
+            },
+        })
+
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if type is not provided', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            body: {
+                ...httpRequest,
+                type: undefined,
             },
         })
 
