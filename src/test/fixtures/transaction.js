3b6da9751d0e628aa5768dd6aff6e105f9244a3c
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-14 20:13:32 -0300

    test: add transaction and user fixtures

---
+++ src/test/fixtures/transaction.js
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker'
+
+export const createTransactionParams = {
+    id: faker.string.uuid(),
+    name: faker.string.alphanumeric(10),
+    user_id: faker.string.uuid(),
+    date: faker.date.anytime().toISOString(),
+    amount: Number(faker.finance.amount()),
+    type: 'EARNING',
+}
