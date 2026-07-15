64ef880681c6158a56f445d54b398589c7946af8
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-15 17:28:26 -0300

    test: ensure CreateUserRepository creates a user on db

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -6,8 +6,12 @@ describe('CreateUserRepository', () => {
     it('should create a user on db', async () => {
         const sut = new PostgresCreateUserRepository()
 
-        const result = sut.execute(user)
+        const result = await sut.execute(user)
 
-        expect(result).toBeTruthy()
+        expect(result.id).toBe(user.id)
+        expect(result.first_name).toBe(user.first_name)
+        expect(result.last_name).toBe(user.last_name)
+        expect(result.email).toBe(user.email)
+        expect(result.password).toBe(user.password)
     })
 })
