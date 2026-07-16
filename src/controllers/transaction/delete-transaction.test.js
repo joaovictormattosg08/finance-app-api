fc94c8329b5b3c119c581a9a81318424ad1f326b
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-16 16:10:35 -0300

    test: ensure DeleteTransactionController throw 404 if TransactionNotFoundError thrown

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 import { DeleteTransactionController } from './delete-transaction'
 import { transactionNotFoundResponse } from '../helpers'
 import { transaction } from '../../test'
+import { TransactionNotFoundError } from '../../errors/transaction'
 
 describe('DeleteTransactionController', () => {
     class DeleteTransactionUseCaseStub {
@@ -47,7 +48,9 @@ describe('DeleteTransactionController', () => {
 
     it('should return 404 when transaction is not found', async () => {
         const { sut, deleteTransactionUseCase } = makeSut()
-        jest.spyOn(deleteTransactionUseCase, 'execute').mockResolvedValue(null)
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new TransactionNotFoundError(),
+        )
 
         const result = await sut.execute(httpRequest)
 
