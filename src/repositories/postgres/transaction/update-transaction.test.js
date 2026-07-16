9e71d28ef42e8a63fbb3d67dfa2f89db58cfeb6b
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-16 15:41:34 -0300

    test: ensure UpdateTransactionRepository calls prisma with correct params

--- src/repositories/postgres/transaction/update-transaction.test.js
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -4,6 +4,14 @@ import { PostgresUpdateTransactionRepository } from './update-transaction'
 import { faker } from '@faker-js/faker'
 import dayjs from 'dayjs'
 
+const params = {
+    id: faker.string.uuid(),
+    name: faker.string.alphanumeric(10),
+    user_id: user.id,
+    date: faker.date.anytime().toISOString(),
+    amount: Number(faker.finance.amount()),
+    type: 'EARNING',
+}
 describe('UpdateTransactionRepository', () => {
     it('should update a transaction on db', async () => {
         await prisma.user.create({ data: user })
@@ -11,15 +19,6 @@ describe('UpdateTransactionRepository', () => {
             data: { ...transaction, user_id: user.id },
         })
         const sut = new PostgresUpdateTransactionRepository()
-
-        const params = {
-            id: faker.string.uuid(),
-            name: faker.string.alphanumeric(10),
-            user_id: user.id,
-            date: faker.date.anytime().toISOString(),
-            amount: Number(faker.finance.amount()),
-            type: 'EARNING',
-        }
 
         const result = await sut.execute(transaction.id, params)
 
@@ -33,5 +32,26 @@ describe('UpdateTransactionRepository', () => {
         )
         expect(dayjs(result.date).month()).toBe(dayjs(params.date).month())
         expect(dayjs(result.date).year()).toBe(dayjs(params.date).year())
+    })
+
+    it('should call prisma with correct params', async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        })
+        const sut = new PostgresUpdateTransactionRepository()
+        const prismaSpy = jest.spyOn(prisma.transaction, 'update')
+
+        await sut.execute(transaction.id, {
+            ...transaction,
+            user_id: user.id,
+        })
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: transaction.id,
+            },
+            data: { ...transaction, user_id: user.id },
+        })
     })
 })
