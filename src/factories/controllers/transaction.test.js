5e1679c5791526512b569b3b6325fbbc73959726
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-16 16:54:17 -0300

    test: ensure makeCreateTransactionController returns valid CreateTransactionController

---
+++ src/factories/controllers/transaction.test.js
@@ -0,0 +1,20 @@
+import {
+    CreateTransactionController,
+    DeleteTransactionController,
+    GetTransactionsByUserIdController,
+    UpdateTransactionController,
+} from '../../controllers'
+import {
+    makeCreateTransactionController,
+    makeDeleteTransactionController,
+    makeGetTransactionsByUserIdController,
+    makeUpdateTransactionController,
+} from './transaction'
+
+describe('Transaction controller Factories', () => {
+    it('should return a valid CreateTransactionController instace', () => {
+        expect(makeCreateTransactionController()).toBeInstanceOf(
+            CreateTransactionController,
+        )
+    })
+})
