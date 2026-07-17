bb397798f57be248c65fd1db8f592800815f1152
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-17 17:55:33 -0300

    refactor: move transactions routes to separate file

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ import {
     CreateTransactionController,
 } from './src/controllers/index.js'
 
-import {
-    makeCreateTransactionController,
-    makeDeleteTransactionController,
-    makeGetTransactionByUserIdController,
-    makeUpdateTransactionController,
-} from './src/factories/controllers/transaction.js'
 import { usersRouter } from './src/routes/user.js'
+import { transactionsRouter } from './src/routes/transactions.js'
 
 const app = express()
 
@@ -22,42 +17,7 @@ app.use(express.json())
 
 app.use('/api/users', usersRouter)
 
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionByUserIdController =
-        makeGetTransactionByUserIdController()
-
-    const { statusCode, body } =
-        await getTransactionByUserIdController.execute(request)
-
-    response.status(statusCode).json(body)
-})
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController()
-
-    const { statusCode, body } =
-        await createTransactionController.execute(request)
-
-    response.status(statusCode).json(body)
-})
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransactionController = makeUpdateTransactionController()
-
-    const { statusCode, body } =
-        await updateTransactionController.execute(request)
-
-    response.status(statusCode).json(body)
-})
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const DeleteTransactionController = makeDeleteTransactionController()
-
-    const { statusCode, body } =
-        await DeleteTransactionController.execute(request)
-
-    response.status(statusCode).json(body)
-})
+app.use('/api/transactions', transactionsRouter)
 // 3000: Porta que será usada para acessar o projeto
 app.listen(process.env.PORT, () =>
     console.log(`Listening on port ${process.env.PORT}`),
---
+++ src/routes/transactions.js
@@ -0,0 +1,51 @@
+import {
+    makeCreateTransactionController,
+    makeDeleteTransactionController,
+    makeGetTransactionByUserIdController,
+    makeUpdateTransactionController,
+} from '../factories/controllers/transaction.js'
+import { Router } from 'express'
+
+export const transactionsRouter = Router()
+transactionsRouter.get('/', async (request, response) => {
+    const getTransactionByUserIdController =
+        makeGetTransactionByUserIdController()
+
+    const { statusCode, body } =
+        await getTransactionByUserIdController.execute(request)
+
+    response.status(statusCode).json(body)
+})
+
+transactionsRouter.post('/', async (request, response) => {
+    const createTransactionController = makeCreateTransactionController()
+
+    const { statusCode, body } =
+        await createTransactionController.execute(request)
+
+    response.status(statusCode).json(body)
+})
+
+transactionsRouter.patch(
+    '/:transactionId',
+    async (request, response) => {
+        const updateTransactionController = makeUpdateTransactionController()
+
+        const { statusCode, body } =
+            await updateTransactionController.execute(request)
+
+        response.status(statusCode).json(body)
+    },
+)
+
+transactionsRouter.delete(
+    '/:transactionId',
+    async (request, response) => {
+        const DeleteTransactionController = makeDeleteTransactionController()
+
+        const { statusCode, body } =
+            await DeleteTransactionController.execute(request)
+
+        response.status(statusCode).json(body)
+    },
+)
