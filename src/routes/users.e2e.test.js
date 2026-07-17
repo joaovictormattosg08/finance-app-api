a1304f584d226ebb33ace86a72de76b86fd8f728
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-17 20:54:21 -0300

    test: ensure GET /api/users/userId/balance returns 200 and user balance

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../app.js'
 import { user } from '../test/fixtures/user.js'
 import { faker } from '@faker-js/faker'
+import { TransactionType } from '@prisma/client'
 
 describe('User Routes E2E Tests', () => {
     it('POST /api/users should return 201 when user is created', async () => {
@@ -69,5 +70,49 @@ describe('User Routes E2E Tests', () => {
 
         expect(response.status).toBe(200)
         expect(response.body).toEqual(createdUser)
+    })
+
+    it('GET /api/users/userId/balance should return 200 and correct balance', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EARNING,
+            amount: 10000,
+        })
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EXPENSE,
+            amount: 3000,
+        })
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.INVESTMENT,
+            amount: 1000,
+        })
+        const response = await request(app).get(
+            `/api/users/${createdUser.id}/balance`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({
+            earnings: '10000',
+            expenses: '3000',
+            investments: '1000',
+            balance: '6000',
+        })
     })
 })
