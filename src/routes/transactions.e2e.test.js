f7ae45b403d8dc3d811ca077e377e6429a6134eb
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-17 22:06:06 -0300

    test: ensure DELETE /api/transactions/:transactionId returns 404 when a transaction is not found

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -106,4 +106,12 @@ describe('Transaction Routes E2E Tests', () => {
 
         expect(response.status).toBe(404)
     })
+
+    it('DELETE /api/transactions/:transactionId should return 404 when deleting a non existing transaction ', async () => {
+        const response = await request(app).delete(
+            `/api/transactions/${faker.string.uuid()}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
 })
