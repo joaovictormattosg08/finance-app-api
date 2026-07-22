e7be6fcaee82719bf461eb123bd2cfed49cb76dd
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-07-22 17:49:25 -0300

    feat: ensure eslint is working

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,25 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-]);
+    js.configs.recommended,
+
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+
+    {
+        files: ['**/*.test.js'],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+            },
+        },
+    },
+])
