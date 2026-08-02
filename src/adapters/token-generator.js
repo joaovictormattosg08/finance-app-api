f54158f7613a9fbaf87b4bc03c93905fe5812188
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-08-01 23:11:25 -0300

    refactor: add login-user use case export and change tokenGenerator returns names

--- src/adapters/token-generator.js
+++ src/adapters/token-generator.js
@@ -3,12 +3,12 @@ import jwt from 'jsonwebtoken'
 export class TokenGeneratorAdapter {
     async execute(userId) {
         return {
-            acessToken: jwt.sign(
+            accessToken: jwt.sign(
                 { userId },
                 process.env.JWT_ACESS_TOKEN_SECRET,
                 { expiresIn: '15m' },
             ),
-            refresh: jwt.sign(
+            refreshToken: jwt.sign(
                 { userId },
                 process.env.JWT_REFRESH_TOKEN_SECRET,
                 { expiresIn: '30d' },
