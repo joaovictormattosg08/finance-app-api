5a3ee1419391f2870bb6b49633271bda1599bfec
joaovictormattosg08 <joaovictorgoncalveskiko@gmail.com>
2026-08-08 16:13:38 -0300

    feat: ensure /balance routes recieves from and to query params and pass it to controlller

--- src/routes/user.js
+++ src/routes/user.js
@@ -67,6 +67,10 @@ usersRouter.get('/balance', auth, async (request, response) => {
         params: {
             userId: request.userId,
         },
+        query: {
+            from: request.query.from,
+            to: request.query.to,
+        },
     })
 
     response.status(statusCode).json(body)
